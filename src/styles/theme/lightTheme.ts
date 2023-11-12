752230e75e5939ef9a722262e26f6a4d3f0cffc6
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-11-12 11:24:05 +0700

    feat: setup mui and implement theming capability

---
+++ src/styles/theme/lightTheme.ts
@@ -0,0 +1,12 @@
+import { Theme, createTheme } from '@mui/material';
+
+const lightTheme: Theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#fcba03',
+    },
+  },
+});
+
+export default lightTheme;
