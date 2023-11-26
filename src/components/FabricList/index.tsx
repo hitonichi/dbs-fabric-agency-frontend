a95754dfaa0b7d2f1609563f577f59c09d95d282
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-11-27 01:33:45 +0700

    fix: add price field for fabric table

--- src/components/FabricList/index.tsx
+++ src/components/FabricList/index.tsx
@@ -3,7 +3,7 @@ import TableComponent from '../TableComponent';
 import { TableCellProps } from '@mui/material';
 
 interface Column {
-  id: 'category' | 'colour' | 'quantity' | 'supplier';
+  id: 'category' | 'colour' | 'quantity' | 'supplier' | 'price';
   label: string;
   minWidth?: number;
   align?: TableCellProps['align'];
@@ -20,25 +20,26 @@ const columns: readonly Column[] = [
     format: (value: number) => value.toFixed(2),
   },
   {
+    id: 'price',
+    label: 'Price',
+    minWidth: 200,
+    align: 'right',
+    format: (value: number) => '$ ' + value.toLocaleString('en-US'),
+  },
+  {
     id: 'quantity',
-    label: 'Quantity',
+    label: 'Quantity (Bolts)',
     minWidth: 200,
     align: 'right',
     format: (value: number) => value.toLocaleString('en-US'),
   },
-  // {
-  //   id: 'bank',
-  //   label: 'Bank account',
-  //   minWidth: 120,
-  //   align: 'center',
-  //   format: (value: number) => value.toLocaleString('en-US'),
-  // },
 ];
 
 interface Data {
   id: string;
   category: string;
   colour: string;
+  price: number;
   quantity: number;
   supplier: string;
 }
@@ -47,21 +48,22 @@ function createData(
   id: string,
   category: string,
   colour: string,
+  price: number,
   quantity: number,
   supplier: string
 ): Data {
-  return { id, category, colour, quantity, supplier };
+  return { id, category, colour, price, quantity, supplier };
 }
 
 const rows = [
-  createData('11111', 'Silk', '#BDB1A8', 1000, 'John Murtough'),
-  createData('11112', 'Jacquard', '#746E50', 1000, 'John Murtough'),
-  createData('11113', 'Damask', '#8B5959', 1000, 'John Murtough'),
-  createData('11114', 'Khaki', '#F0E68C', 1000, 'John Murtough'),
-  createData('11115', 'Silk', '#B7A99B', 1000, 'John Murtough'),
-  createData('11116', 'Faux silk', '#FFBF00', 1000, 'John Murtough'),
-  createData('11117', 'Crewel', '#CBB99B', 1000, 'John Murtough'),
-  createData('11118', 'Damask', '#FCF2DF', 1000, 'John Murtough'),
+  createData('11111', 'Silk', '#BDB1A8', 1234, 1000, 'John Murtough'),
+  createData('11112', 'Jacquard', '#746E50', 1234, 1000, 'John Murtough'),
+  createData('11113', 'Damask', '#8B5959', 1234, 1000, 'John Murtough'),
+  createData('11114', 'Khaki', '#F0E68C', 1234, 1000, 'John Murtough'),
+  createData('11115', 'Silk', '#B7A99B', 1234, 1000, 'John Murtough'),
+  createData('11116', 'Faux silk', '#FFBF00', 1234, 1000, 'John Murtough'),
+  createData('11117', 'Crewel', '#CBB99B', 1234, 1000, 'John Murtough'),
+  createData('11118', 'Damask', '#FCF2DF', 1234, 1000, 'John Murtough'),
 ];
 
 export default function FabricList() {
